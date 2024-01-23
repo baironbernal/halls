b22504f5e547ed44e315b52f457ea37111b0e19f
baironbernal <baironbernal263@gmail.com>
2024-01-23 12:23:43 -0500

    adjust links and pieces images

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Button.css';
-import { Link } from 'react-router-dom/cjs/react-router-dom.min';
+
 
 const Button = ({ url, titleButton, borderColor }) => {
   return (
@@ -10,9 +10,9 @@ const Button = ({ url, titleButton, borderColor }) => {
           <button className='button-general rounded-5 px-4 py-2' style={{
           borderColor: borderColor
         }}>
-          <Link to={url} className='text-white text-decoration-none z-1'>
+          <a href={url} className='text-white text-decoration-none z-1'>
             {titleButton}
-            </Link>  
+            </a>  
           </button>
         ): 
         <button className='button-general rounded-5 px-4 py-2' style={{
